70ec07dca09a35f2d0810079c1c38e751326c6e5
kcspex <kcspex@gmail.com>
2024-09-23 11:57:27 +0100

    Updated

--- phpscript/get-staff-signature.php
+++ phpscript/get-staff-signature.php
@@ -1,8 +1,8 @@
 <?php
     include ('../database/config.php');
-    
+
     $id = $_POST['id'];
-    
+
     $sqlexamsubjects = "SELECT * FROM `staffsignature` WHERE staff_id= '$id'";
     $resultexamsubjects = mysqli_query($link, $sqlexamsubjects);
     $rowexamsubjects = mysqli_fetch_assoc($resultexamsubjects);
@@ -10,11 +10,26 @@
 
     if($row_cntexamsubjects > 0)
     {
-        
-            echo'<div align="center" style="padding:5%;">
-                    <img src="../img/signature/'.$rowexamsubjects['Signature'].'" width="20%" height="auto"/>
-                </div>';
-            
+            // Get the full request URI
+            $requestUri = $_SERVER['REQUEST_URI'];
+
+            // Remove the leading `/` if needed and remove query parameters if present
+            $path = parse_url($requestUri, PHP_URL_PATH);
+
+            // Display the result
+            // echo $path;  // Output: /admin/settingTeacherDefaultComments.php
+            if ($path == "admin/settingTeacherDefaultComments.php")
+            {
+              echo'<div align="center" style="padding:5%;">
+                      <img src="../img/signature/'.$rowexamsubjects['Signature'].'" width="20%" height="auto"/>
+                  </div>';
+            } else {
+              echo'<div align="center" style="padding:5%;">
+                      <img src="'. 'https://schoollift.s3.us-east-2.amazonaws.com/'. $rowexamsubjects['Signature'].'" width="20%" height="auto"/>
+                  </div>';
+            }
+
+
     }
     else
     {
