1f3622a28910dda3f174beb26e3b6a7ec17d8eb5
kcspex <kcspex@gmail.com>
2024-09-23 13:21:19 +0100

    Updated

--- phpscript/get-staff-signature.php
+++ phpscript/get-staff-signature.php
@@ -2,7 +2,7 @@
     include ('../database/config.php');
 
     $id = $_POST['id'];
-    $currentPath = $_POST['currentPath'];
+    $uri = $_POST['uri'];
 
     $sqlexamsubjects = "SELECT * FROM `staffsignature` WHERE staff_id= '$id'";
     $resultexamsubjects = mysqli_query($link, $sqlexamsubjects);
@@ -25,7 +25,7 @@
                       <img src="../img/signature/'.$rowexamsubjects['Signature'].'" width="20%" height="auto"/>
                   </div>';
             } else {
-              echo $currentPath;
+              echo $uri;
               echo'<div align="center" style="padding:5%;">
                       <img src="'. 'https://schoollift.s3.us-east-2.amazonaws.com/'. $rowexamsubjects['Signature'].'" width="20%" height="auto"/>
                   </div>';
