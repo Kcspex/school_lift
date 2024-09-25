d25d4a6e552b5c653fa3fc5c0b67fd453164a144
kcspex <kcspex@gmail.com>
2024-09-25 10:14:08 +0100

    Updated

--- admin/schoolHeadDefaultComment.php
+++ admin/schoolHeadDefaultComment.php
@@ -1,11 +1,5 @@
 <?php
 include ('../database/config.php');
-
-// Turn on error reporting
-error_reporting(E_ALL);
-
-// Display errors on the web page
-ini_set('display_errors', 1);
 ?>
 <!doctype html>
 <html lang="en">
--- phpinfo.php
+++ phpinfo.php
@@ -1,4 +1,9 @@
 <?php
+// Turn on error reporting
+error_reporting(E_ALL);
+
+// Display errors on the web page
+ini_set('display_errors', 1);
 $xml = new SimpleXMLElement('<root><test>Hello World</test></root>');
 echo $xml->test;
 ?>
