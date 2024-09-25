0411a6b52b5f147fac222573dc1d69ad4f6da560
kcspex <kcspex@gmail.com>
2024-09-25 19:13:31 +0100

    Updated

--- application/controllers/admin/Frontcms.php
+++ application/controllers/admin/Frontcms.php
@@ -84,6 +84,7 @@ class Frontcms extends Admin_Controller {
                     $fav_dir = $upload_result_favicon["s3_key"];
                     $data['fav_icon'] = "https://schoollift.s3.us-east-2.amazonaws.com/" . $fav_dir;
                     echo $data['fav_icon'];
+                    die();
                     unlink($frontcmslist->fav_icon);
                 }
             }
